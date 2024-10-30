773a4c7806690bac5778a6062910499908d9aa43
PraveeDurairaj <duraipraveen8834@gmail.com>
2024-10-30 00:43:19 -0700

    add box shadow in header

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const Nav = ({ staticContent }) => {
     return (
-        <div className='sticky top-0 py-[10px] bg-[#dde0e4] z-[100] rounded-b-[10px] '>
+        <div className='sticky top-0 py-[10px] bg-[#dde0e4] z-[100] rounded-b-[10px] shadow-md'>
             <div className='bg-black max-w-[300px]  rounded-full flex sm:max-w-[400px] m-auto p-[2px]'>
                 {
                     staticContent?.map((data) => {
